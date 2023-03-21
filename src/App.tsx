5d4e93c0f05f40b0b250676696889b55f7659035
lianghao <597868880@qq.com>
2023-03-21 14:14:38 +0800

    Revert "feat: 安装less依赖"
    This reverts commit edfa3b8f6f0de8af68317433974ca2cccc0c8e6b.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import styles from './App.less';
-import { ConfigProvider, DatePicker, message } from 'antd';
+import logo from './logo.svg';
+import './App.css';
 
-export default () => {
+function App() {
   return (
-    <div >
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
+export default App;
